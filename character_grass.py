8fd05724c279f0a2a2ff28a39e3b0b15e68f8652
kimi5231 <djwjod@naver.com>
2023-09-21 11:52:12 +0900

    원 운동, 사각형 운동 프레임 완성

--- character_grass.py
+++ character_grass.py
@@ -10,4 +10,19 @@ grass.draw_now(400,30)
 character.draw_now(400,90)
 delay(1)
 
+
+def run_circle():
+    print('CIRCLE')
+    pass
+
+
+def run_rectangle():
+    print('RECTANGLE')
+    pass
+
+
+while True:
+    run_circle()
+    run_rectangle()
+
 close_canvas()
